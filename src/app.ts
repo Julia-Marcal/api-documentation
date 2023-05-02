89de95b8b3441df88830a2e2e37a938fd957239f
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-05-02 11:54:08 -0300

    setting swagger with the server

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,38 @@
 import express from 'express';
 import cors from 'cors';
+
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+
 const router = require('./routes/router')
 
 const app = express()
 
+
+const definition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Valo Agents API',
+    version: '1.0.0',
+    description: 'Documenting the valorant agent api'
+  },
+};
+
+const options = {
+  definition,
+  servers: [{
+    url: '<http://localhost:4000>',
+    description: 'Local server'
+  }],
+  apis: ['src/controllers/*.ts']
+}
+
+const swaggerSpec = swaggerJsdoc(options)
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
 app.use(express.json())
+
 app.use(cors())
 
 app.use(router)
