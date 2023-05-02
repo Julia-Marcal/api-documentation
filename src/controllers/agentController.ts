0294f5de2f6a6726e52e9b2f261ab66ffb1f966c
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-05-02 11:54:41 -0300

    Documenting the post method of agents

--- src/controllers/agentController.ts
+++ src/controllers/agentController.ts
@@ -2,6 +2,48 @@ import { createAgent } from '../repositories/agentCreate'
 import { Request, Response } from 'express'
 import {agentValidation} from '../validation/validation'
 
+/**
+ * Create a new agent to the postgres db
+ *
+ * @swagger
+ * /agents:
+ *  post:
+ *    description: Insert a new agent to the db.
+ *    parameters:
+ *      - name: name
+ *        in: body
+ *        description: Name of the agent
+ *        required: true
+ *        schema:
+ *          type: string
+ *          minimum: 1
+ *      - name: real_name
+ *        in: body
+ *        description: Real name of the agent
+ *        required: true
+ *        schema:
+ *          type: string
+ *          minimum: 1
+ *      - name: age
+ *        in: body
+ *        description: Age of tha agent
+ *        required: true
+ *        schema:
+ *          type: number
+ *      - name: country
+ *        in: body
+ *        description: Country of the agent
+ *        required: true
+ *        schema:
+ *          type: string
+ *          minimum: 1
+ *    responses:
+ *      200:
+ *        description: Agent was created
+ *      400:
+ *        description: Bad request
+ */
+
 export const create = async(req: Request, res: Response) => {
   try{
     await agentValidation.parse(req.body)
