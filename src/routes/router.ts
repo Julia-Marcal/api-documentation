fb7a278b5cea9ec528c934c1f8467cd423cc7dc6
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-05-02 16:17:20 -0300

    adding more routes

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 const Hellocontroller = require('../controllers/basicController')
 const AgentCreateController = require('../controllers/agentPostController')
 const AgentViewAllController = require('../controllers/agentViewAllController')
+const ViewAgentById = require('../controllers/agentViewOneController')
 
 const router = Router()
 
 router.get('/HelloWorld', Hellocontroller.hello);
 router.get('/', AgentViewAllController.viewAllAgents)
+router.get('/:id', ViewAgentById.findById)
 router.post('/', AgentCreateController.create);
 
 module.exports = router;
