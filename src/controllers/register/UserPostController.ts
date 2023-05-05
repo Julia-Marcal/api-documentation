687cf82ef1bb73e2f9b77513fddf179ea9098bd0
Julia-Marcal <anajulia.05marcal@gmail.com>
2023-05-05 11:37:16 -0300

    Controller to create users

---
+++ src/controllers/register/UserPostController.ts
@@ -0,0 +1,25 @@
+import { createUser } from '../../repositories/userCreate'
+import { Request, Response} from 'express'
+import {userValidation} from '../../validation/validationUser'
+import { prisma } from '../../services/prisma'
+import jwt from 'jsonwebtoken';
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+const my_secret = String(process.env.SECRET)
+
+
+export const create = async(req: Request, res: Response) => {
+  try{
+    await userValidation.parse(req.body)
+    const user = await createUser(req.body);
+    const token = jwt.sign({user}, my_secret, {expiresIn: '1y'})
+    await prisma.$disconnect
+    res.status(200).send({user, token})
+  }catch (e){
+    res.status(400).send(e)
+  }
+}
+
+
